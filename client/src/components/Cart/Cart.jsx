5e9f6fd77021db296f6a670193f2e4419639e285
Souradip Adhikari <souradip.cloud@gmail.com>
2023-07-13 22:57:18 +0530

    Day-4

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -2,12 +2,25 @@ import "./Cart.scss";
 import {MdClose} from "react-icons/md"
 import {BsCartX} from "react-icons/bs"
 
-const Cart = () => {
+const Cart = ({setShowCart}) => {
     return (
         <div className="cart-pannel">
             <div className="opac-layer"></div>
             <div className="cart-content">
-                ghjkjhghjk
+                <div className="cart-header">
+                    <span className="heading">
+                        Your Gardening Items
+                    </span>
+                    <span className="close-btn" onClick={()=>setShowCart(false)}>
+                        <MdClose/>
+                        <span className="text">Close</span>
+                    </span>
+                </div>
+                <div className="empty-cart">
+                    <BsCartX/>
+                    <span>No Items For Your Garden</span>
+                    <button className="return-btn">Return</button>
+                </div>
             </div>
         </div>
     );
