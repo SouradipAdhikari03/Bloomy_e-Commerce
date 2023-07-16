8bb5460ff0f4d4808fd395a8780c0817074fe737
Souradip Adhikari <souradip.cloud@gmail.com>
2023-07-16 20:57:41 +0530

    Day-7

--- client/src/components/Header/Search/Search.jsx
+++ client/src/components/Header/Search/Search.jsx
@@ -1,6 +1,34 @@
 import "./Search.scss";
-const Search = () => {
-    return <div>Search</div>;
+import prod from "../../../assets/products/snake-plant.webp"
+
+import {MdClose} from "react-icons/md"
+const Search = ({setshowSearch}) => {
+    return(
+        <div className="search-modal">
+            <div className="form-field">
+                <input 
+                    type="text"
+                    autoFocus
+                    placeholder="Search for your garden"
+                />
+                <MdClose onClick={()=>setshowSearch(false)} className="close-btn"/>
+            </div>
+            <div className="search-result-content">
+                <div className="search-results">
+                    <div className="search-result-item">
+                        <div className="img-container">
+                            <img src={prod} alt="" />
+                        </div>
+                        <div className="prod-details">
+                            <div className="name">Product name</div>
+                            <div className="desc">Product desc</div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    );
 };
 
 export default Search;
