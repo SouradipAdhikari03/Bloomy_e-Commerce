b7ad1201be310b0ddd5eb3850c3016d587c7ebe7
Souradip Adhikari <souradip.cloud@gmail.com>
2023-07-18 00:46:45 +0530

    Day-8

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,19 +1,30 @@
+import { useEffect } from "react";
+import { fetchDataFromApi } from "../../utils/api";
+
 import "./Home.scss";
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Product from "../Products/Products";
 const Home = () => {
-    return (
-        <div>
-            <Banner/>
-            <div className="main-content">
-                <div className="layout">
-                    <Category/>
-                    <Product headingText="Trending Products"/>
-                </div>
-            </div>
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  const getCategories = () => {
+    fetchDataFromApi("/api/categories/?populate=*").then((res) => console.log(res));
+  };
+
+  return (
+    <div>
+      <Banner />
+      <div className="main-content">
+        <div className="layout">
+          <Category />
+          <Product headingText="Trending Products" />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
